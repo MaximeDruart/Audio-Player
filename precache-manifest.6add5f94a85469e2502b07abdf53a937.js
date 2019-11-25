9409bbdc3d9143bcea87c89d1bd06d337ba51604
Maxime Druart <maxime.druart@yahoo.fr>
2019-11-25 08:19:21 +0100

    Updates

---
+++ precache-manifest.6add5f94a85469e2502b07abdf53a937.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2a6f70326d63d242bb56c7ec7323c4",
+    "url": "/Audio-Player/index.html"
+  },
+  {
+    "revision": "850a3d86ee688fe9379a",
+    "url": "/Audio-Player/static/css/main.ba66b4a8.chunk.css"
+  },
+  {
+    "revision": "b9064f5e2c0c9fcc68bd",
+    "url": "/Audio-Player/static/js/2.7f2f8ac8.chunk.js"
+  },
+  {
+    "revision": "850a3d86ee688fe9379a",
+    "url": "/Audio-Player/static/js/main.a9dc468a.chunk.js"
+  },
+  {
+    "revision": "16695b60b320a0b17aa1",
+    "url": "/Audio-Player/static/js/runtime-main.f45643d4.js"
+  },
+  {
+    "revision": "5fcd6c657ffa8a6791632c705f0944ea",
+    "url": "/Audio-Player/static/media/VCR_OSD_MONO_1.001.5fcd6c65.ttf"
+  }
+]);
